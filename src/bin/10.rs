832970927aff9263132ad45eeaac336789330285
Jasper Ty <jasperty2@gmail.com>
2024-12-10 13:29:41 -0500

    Solve 10

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,11 +1,66 @@
 advent_of_code::solution!(10);
 
+use advent_of_code::Map;
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let map = Map::new(input);
+
+    let mut stack: Vec<(u8, (usize, usize))> = vec![];
+    let mut sum = 0;
+    let mut traversed = Map::empty(map.width(), map.height());
+
+    for i in 0..map.data.len() {
+        stack.clear();
+        traversed.clear();
+        if map.data[i] == b'9' {
+            stack.push((b'9', map.pos(i)));
+            while let Some((level, (x, y))) = stack.pop() {
+                traversed[(x, y)] = b'X';
+                if level == b'0' {
+                    sum += 1;
+                } else {
+                    for dir in [(1, 0), (0, 1), (-1, 0), (0, -1)] {
+                        if let Some((x, y)) = map.step((x, y), dir) {
+                            if map[(x, y)] == level - 1 && traversed[(x, y)] != b'X' {
+                                stack.push((level - 1, (x, y)));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Some(sum)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let map = Map::new(input);
+
+    let mut stack: Vec<(u8, (usize, usize))> = vec![];
+    let mut sum = 0;
+
+    for i in 0..map.data.len() {
+        if map.data[i] == b'9' {
+            stack.push((b'9', map.pos(i)));
+        }
+    }
+
+    while let Some((level, (x, y))) = stack.pop() {
+        if level == b'0' {
+            sum += 1;
+        } else {
+            for dir in [(1, 0), (0, 1), (-1, 0), (0, -1)] {
+                if let Some((x, y)) = map.step((x, y), dir) {
+                    if map[(x, y)] == level - 1 {
+                        stack.push((level - 1, (x, y)));
+                    }
+                }
+            }
+        }
+    }
+
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -15,12 +70,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(36));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(81));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,80 @@
 pub mod template;
 
 // Use this file to add helper functions and additional modules.
+
+pub struct Map {
+    pub data: Vec<u8>,
+    width: usize,
+    height: usize,
+}
+impl Map {
+    pub fn empty(width: usize, height: usize) -> Self {
+        Self {
+            data: vec![b'.'; width * height],
+            width,
+            height
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.data.fill(b'.');
+    }
+
+    pub fn new(input: &str) -> Self {
+        Self {
+            data: input.lines().flat_map(str::bytes).collect(),
+            width: input.lines().next().unwrap().len(),
+            height: input.lines().count()
+        }
+    }
+
+    pub fn step(&self, (x, y): (usize, usize), (dx, dy): (i32, i32)) -> Option<(usize, usize)> {
+        let x = x as i32 + dx;
+        let y = y as i32 + dy;
+        (x >= 0 && x < self.width as i32 && y >= 0 && y < self.height as i32)
+            .then(|| (x as usize, y as usize))
+    }
+
+    pub fn pos(&self, idx: usize) -> (usize, usize) {
+        (idx % self.width, idx / self.width)
+    }
+    pub fn idx(&self, (x, y): (usize, usize)) -> usize {
+        y * self.width + x
+    }
+
+    pub fn width(&self) -> usize { self.width }
+    pub fn height(&self) -> usize { self.height }
+
+    pub fn get(&self, (x, y): (usize, usize)) -> Option<&u8> {
+        (x < self.width && y < self.height)
+            .then(|| &self[(x, y)])
+    }
+
+    pub fn get_mut(&mut self, x: usize, y: usize) -> Option<&mut u8> {
+        (x < self.width && y < self.height)
+            .then(|| &mut self[(x, y)])
+    }
+
+    pub fn to_str(&self) -> Option<String> {
+        use std::str;
+        self.data[..]
+            .chunks(self.width)
+            .map(|slice| str::from_utf8(slice).ok())
+            .collect::<Option<Vec<&str>>>()
+            .map(|v| v.join("\n"))
+    }
+}
+
+use std::{iter::once, ops::{Index, IndexMut}};
+impl Index<(usize, usize)> for Map {
+    type Output = u8;
+
+    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
+        &self.data[self.width * y + x]
+    }
+}
+impl IndexMut<(usize, usize)> for Map {
+    fn index_mut(&mut self, (x, y): (usize, usize)) -> &mut Self::Output {
+        &mut self.data[self.width * y + x]
+    }
+}
